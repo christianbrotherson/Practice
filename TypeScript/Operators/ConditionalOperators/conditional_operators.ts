a2deb852e626ddb146852dac9eab3033879af945
Christian Brotherson <christian.brotherson@gmail.com>
2017-04-03 13:07:23 -0600

    Practiced with condition operators

--- TypeScript/Operators/ConditionalOperators/conditional_operators.ts
+++ TypeScript/Operators/ConditionalOperators/conditional_operators.ts
@@ -1,29 +1,30 @@
-let x : number = 100;
+// let x : number = 100;
 
-// if(x == 100) {
+// // if(x == 100) {
+// //   console.log("Condition passed");
+// // } 
+
+// // if(x === 100) {
+// //   console.log("Condition passed");
+// // } 
+
+// // if(x != 100) {
+// //   console.log("Condition passed");
+// // } 
+
+// // if(x > 100) {
+// //   console.log("Condition passed");
+// // } 
+
+// if(x >= 100) {
 //   console.log("Condition passed");
 // } 
 
-// if(x === 100) {
+// if(x < 100) {
 //   console.log("Condition passed");
 // } 
 
-// if(x != 100) {
+// if(x <= 100) {
 //   console.log("Condition passed");
 // } 
 
-// if(x > 100) {
-//   console.log("Condition passed");
-// } 
-
-if(x >= 100) {
-  console.log("Condition passed");
-} 
-
-if(x < 100) {
-  console.log("Condition passed");
-} 
-
-if(x <= 100) {
-  console.log("Condition passed");
-} 
