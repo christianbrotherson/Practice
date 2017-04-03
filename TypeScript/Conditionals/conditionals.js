266829772994997316761794e57cfe66ce94f496
Christian Brotherson <christian.brotherson@gmail.com>
2017-04-03 11:28:10 -0600

    Practiced with TS conditionals

---
+++ TypeScript/Conditionals/conditionals.js
@@ -0,0 +1,10 @@
+var password = "zxcvzxcv";
+if (password == "asdfasdf") {
+    console.log("Yes, asdfasdf is the password");
+}
+else if (password == "zxcvzxcv") {
+    console.log("Yes, zxcvzxcv is the password");
+}
+else {
+    console.log("Sorry, that is the incorrect password");
+}
