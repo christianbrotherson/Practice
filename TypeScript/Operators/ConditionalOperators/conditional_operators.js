649e80a012b4e6f91257198b6459f8d5ebc9b3df
Christian Brotherson <christian.brotherson@gmail.com>
2017-04-03 13:06:21 -0600

    Practiced with conditional operators

--- TypeScript/Operators/ConditionalOperators/conditional_operators.js
+++ TypeScript/Operators/ConditionalOperators/conditional_operators.js
@@ -1,29 +1,23 @@
-let x : number = 100;
-
+var x = 100;
 // if(x == 100) {
 //   console.log("Condition passed");
 // } 
-
 // if(x === 100) {
 //   console.log("Condition passed");
 // } 
-
 // if(x != 100) {
 //   console.log("Condition passed");
 // } 
-
 // if(x > 100) {
 //   console.log("Condition passed");
 // } 
-
-if(x >= 100) {
-  console.log("Condition passed");
-} 
-
-if(x < 100) {
-  console.log("Condition passed");
-} 
-
-if(x <= 100) {
-  console.log("Condition passed");
-} 
+if (x >= 100) {
+    console.log("Condition passed");
+}
+if (x < 100) {
+    console.log("Condition passed");
+}
+if (x <= 100) {
+    console.log("Condition passed");
+}
+//# sourceMappingURL=conditional_operators.js.map
