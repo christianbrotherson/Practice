35f9d83dfc47369708a7785687bdc8d6df842da2
Christian Brotherson <christian.brotherson@gmail.com>
2017-04-03 11:22:13 -0600

    Restructured files

---
+++ TypeScript/HelloWorld/hello_world.js
@@ -0,0 +1,6 @@
+var HelloTypeScriptComponent = (function () {
+    function HelloTypeScriptComponent() {
+        this.greeting = 'Hello TypeScript!';
+    }
+    return HelloTypeScriptComponent;
+}());
---
+++ TypeScript/TSVariables/variables.js
@@ -0,0 +1,16 @@
+// var fullName : string = "Christian Brotherson";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+// fullName = "Courtney Brotherson";
+// paidAccount = false;
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+function printName(f, l) {
+    var greeting = "Hi there, ";
+    console.log(greeting + f + " " + l);
+    var greeting = "Hey there, ";
+    console.log(greeting + f + " " + l);
+}
+printName("Chris", "Brotherson");
+//# sourceMappingURL=variables.js.map
