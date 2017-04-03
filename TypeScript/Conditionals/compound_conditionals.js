4413ecf329070b7e996c0a3ac23146a2d2abb18d
Christian Brotherson <christian.brotherson@gmail.com>
2017-04-03 13:21:03 -0600

    Practiced with compound conditionals

---
+++ TypeScript/Conditionals/compound_conditionals.js
@@ -0,0 +1,17 @@
+var password = "zxcvzxcv";
+var password2 = "asdfasdf";
+var email = "test@test.com";
+// if(password == "zxxvzxcv" && email == "test@test.com") {
+//   console.log("You are logged in");
+// } else {
+//   console.log("Sorry, the password and/or email is incorrect");
+// }
+// if(password == "zxcvzxcv" || password == "asdfasdf") {
+//   console.log("You are logged in");
+// } else {
+//   console.log("Sorry, the password and/or email is incorrect");
+// }
+if (!(email == 'test@test.com')) {
+    console.log("You are authorized");
+}
+//# sourceMappingURL=compound_conditionals.js.map
---
+++ TypeScript/Conditionals/compound_conditionals.ts
@@ -0,0 +1,19 @@
+let password : string = "zxcvzxcv";
+let password2 : string = "asdfasdf";
+let email : string = "test@test.com"
+
+// if(password == "zxxvzxcv" && email == "test@test.com") {
+//   console.log("You are logged in");
+// } else {
+//   console.log("Sorry, the password and/or email is incorrect");
+// }
+
+// if(password == "zxcvzxcv" || password == "asdfasdf") {
+//   console.log("You are logged in");
+// } else {
+//   console.log("Sorry, the password and/or email is incorrect");
+// }
+
+if (!(email == 'test@test.com')) {
+  console.log("You are authorized");
+}
