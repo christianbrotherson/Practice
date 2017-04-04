14685c288e3afa1644c6db5326c72eb3d239c11a
Christian Brotherson <christian.brotherson@gmail.com>
2017-04-04 12:35:34 -0600

    Practiced with basic TS functions

---
+++ TypeScript/Functions/functions.js
@@ -0,0 +1,27 @@
+// function fullName(first, last) {
+//   return first + " " + last;
+// }
+// console.log(fullName('Christian', 'Brotherson'));
+function gradeGenerator(grade) {
+    if (grade < 60) {
+        return 'F';
+    }
+    else if (grade >= 60 && grade < 70) {
+        return 'D';
+    }
+    else if (grade >= 70 && grade < 80) {
+        return 'C';
+    }
+    else if (grade >= 80 && grade < 90) {
+        return 'B';
+    }
+    else {
+        return 'A';
+    }
+}
+console.log(gradeGenerator(45));
+console.log(gradeGenerator(100));
+console.log(gradeGenerator(80));
+console.log(gradeGenerator(79));
+console.log(gradeGenerator(61));
+//# sourceMappingURL=functions.js.map
